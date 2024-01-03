0cc6a91b8e4d69d4028fdcdb778cada94355660a
Lewis Revill <lewis.revill@embecosm.com>
2024-01-03 14:27:40 +0000

    Add the ability to build for both FreeBSD and Linux Morello targets
    This commit modifies just the clone, checkout and build scripts so that
    by default they allow building a toolchain for both FreeBSD and Linux
    Morello targets. To disable either, use --no-{freebsd,linux} as an
    argument to each script, or --{freebsd,linux} to re-enable.

--- scripts/clone.sh
+++ scripts/clone.sh
@@ -3,11 +3,46 @@
 
 # Variables used in this script
 TOPDIR="$(realpath $(dirname $0)/../../)"
+LINUX_TARGET=yes
+FREEBSD_TARGET=yes
+
+for opt in ${@}; do
+  valid_arg=1
+  case ${opt} in
+  "--linux")
+    LINUX_TARGET=yes
+    ;;
+  "--no-linux")
+    LINUX_TARGET=no
+    ;;
+  "--freebsd")
+    FREEBSD_TARGET=yes
+    ;;
+  "--no-freebsd")
+    FREEBSD_TARGET=no
+    ;;
+  "--help")
+    valid_arg=0
+    ;;&
+  *)
+    echo "Usage for $0:"
+    echo "  --linux                      Assume we're building a toolchain for Morello Linux [Default]"
+    echo "  --no-linux                   Assume we're not building a toolchain for Morello Linux"
+    echo "  --freebsd                    Assume we're building a toolchain for Morello FreeBSD [Default]"
+    echo "  --no-freebsd                 Assume we're not building a toolchain for Morello FreeBSD"
+    echo "  --help                       Present this message."
+    exit $valid_arg
+    ;;
+  esac
+done
 
 if [ ! -d ${TOPDIR}/toolchain ]; then
   git clone https://github.com/lewis-revill/rust-cheri-toolchain.git ${TOPDIR}/toolchain
 fi
-if [ ! -d ${TOPDIR}/cheribuild ]; then
+if [ "${LINUX_TARGET}" == "yes" && ! -d ${TOPDIR}/toolchain ]; then
+  git clone https://github.com/lewis-revill/morello-sdk.git ${TOPDIR}/morello-sdk
+fi
+if [ "${FREEBSD_TARGET}" == "yes" && ! -d ${TOPDIR}/cheribuild ]; then
   git clone https://github.com/CTSRD-CHERI/cheribuild.git ${TOPDIR}/cheribuild
 fi
 if [ ! -d ${TOPDIR}/rust ]; then
