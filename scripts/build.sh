19f10e60471251900749e7a518d7a8cffee95971
Lewis Revill <lewis.revill@embecosm.com>
2024-06-04 15:07:01 +0100

    Add more downgrading of dependencies in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -84,6 +84,9 @@ fi
   cargo update -p tinystr@0.7.5 --precise 0.7.1
   cargo update -p tracing-tree@0.2.5 --precise 0.2.4
   cargo update -p home@0.5.9 --precise 0.5.5
+  cargo update -p thiserror@1.0.60 --precise 1.0.59
+  cargo update -p serde_json@1.0.117 --precise 1.0.116
+  cargo update -p mdbook@0.4.37 --precise 0.4.36
   (
     cd src/bootstrap
     cargo update -p home@0.5.9 --precise 0.5.5
